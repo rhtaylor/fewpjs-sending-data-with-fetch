edb3479791fd3bb3e88c15de8b5b23a7c8379f02
Randy <randyisbusy@gmail.com>
2020-02-22 22:00:35 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Add your code here
+function submitData(name, email){  
+    
+    const userInfo = { name: name , email: email}
+    
+    const configureObj = {  
+        "method": "POST",
+        headers: {  
+             
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userInfo)
+    };
+ 
+    return fetch('http://localhost:3000/users', configureObj)
+        .then(function (response) { 
+            
+            return response.json();
+        })
+        .then( data => {  
+            let xyz = document.getElementById("yo");
+            let zzz = document.createElement("h1"); 
+            zzz.innerHTML = data["id"] + " " + data["name"] + " " + data["email"] ;
+            xyz.appendChild(zzz);  
+            })   
+        .then(data => function(resolve){ 
+              
+            resolve(data)  
+        }) 
+        .catch(function (error) {  
+             debugger
+            if (error){
+            let xyz = document.getElementById("yo");
+            error.message = "Unauthorized Access"; 
+            
+            xyz.innerHTML = error.message}
+                 
+        });
+
+    } 
+
